dc0b58aa764d56c5b7b25fd4710e8905f090a247
徐泽宇 <xzy@9elephas.com>
2017-11-06 13:45:31 +0800

    完成 把 客户的合同列表迁移到 客户列表界面里面

--- src/main/resources/static/assets/js/customer/edit.js
+++ src/main/resources/static/assets/js/customer/edit.js
@@ -1,43 +1,6 @@
 $().ready(function () {
 
-    // 显示此客户对应的合同列表
-    if ($("#contract_table").length > 0) {
-        contract_table = $("#contract_table").DataTable({
-            "width": "100%",
-            "autoWidth": true,
-            "ajax": {
-                url: contextPath + "contracts/" + $("#id").val() + "/getDataWithoutPaged.json",
-                error: function (data, textStatus, jqXHR) {
-                    showMessage("danger", "错误", jqXHR.responseJSON.data[0].errorMessage);
-                },
-            },
-            "language": {
-                "url": contextPath + "assets/js/lib/DataTables-1.10.16/chinese.lang.json"
-            },
-            "columns": [{
-                "data": "name"
-            }, {
-                "data": "amount"
-            }, {
-                "data": "beginDate"
-            }, {
-                "data": "endDate"
-            },{
-                "data": "firstGatherDate"
-            }],
-            "order": [[ 2, "desc" ]],
-            "columnDefs": [
-                {
-                    "orderable": false,
-                    "targets": [5],
-                    "render": function (data, type, row, meta) {
-                        // return '<button type="button" class="btn btn-outline-primary"  onclick="fun_edit(\'' + row.id + '\')">编辑</button>&nbsp;&nbsp;<button class="btn btn-outline-danger btn-sm" type="button"  onclick="fun_delete(\'' + row.id + '\')">删除</button>'
-                        return '<a href="javascript:fun_contract_edit(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="编辑"><i class="la la-edit"></i></a>' +
-                            '<a href="javascript:fun_contract_delete(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title=" 删除"><i class="la la-trash"></i></a>'
-                    }
-                }],
-        });
-    }
+
 });
 
 
--- src/main/resources/static/assets/js/customer/index.js
+++ src/main/resources/static/assets/js/customer/index.js
@@ -1,4 +1,5 @@
 var customer_table;
+var customer_id ;
 
 $().ready(function () {
     // 设置 datatables 的错误，不做抛出。以便接管错误信息
@@ -9,7 +10,7 @@ $().ready(function () {
         "autoWidth": true,
         "ajax": {
             url: contextPath + "customers/getDataWithoutPaged.json",
-            error: function ( jqXHR, textStatus, errorThrown ) {
+            error: function (jqXHR, textStatus, errorThrown) {
                 showMessage("danger", "错误", jqXHR.responseJSON.data[0].errorMessage);
                 mApp.unblock();
             },
@@ -22,7 +23,7 @@ $().ready(function () {
             "data": "name"
         }, {
             "data": "contacts"
-        },{
+        }, {
             "data": "type.name"
         }],
         "columnDefs": [
@@ -31,17 +32,24 @@ $().ready(function () {
                 "targets": [3],
                 "render": function (data, type, row, meta) {
                     // return '<button type="button" class="btn btn-outline-primary"  onclick="fun_edit(\'' + row.id + '\')">编辑</button>&nbsp;&nbsp;<button class="btn btn-outline-danger btn-sm" type="button"  onclick="fun_delete(\'' + row.id + '\')">删除</button>'
-                    return '<a href="javascript:fun_edit(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="编辑"><i class="la la-edit"></i></a>' +
+                    return '<a href="javascript:fun_show_modal(\'' + row.id + '\')"  class="m-portlet__nav-link btn m-btn m-btn--hover-info m-btn--icon m-btn--icon-only m-btn--pill" title=" 合同"><i class="la flaticon-list"></i></a>' +
+                        '<a href="javascript:fun_edit(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="编辑"><i class="la la-edit"></i></a>' +
                         '<a href="javascript:fun_delete(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title=" 删除"><i class="la la-trash"></i></a>'
+
                 }
             }],
     });
 
     $("#addCustomerLink").on("click", function (e) {
-            $.post(contextPath + "events", function (data) {
-                Logger.debug(data);
+        $.post(contextPath + "events", function (data) {
+            Logger.debug(data);
         });
     })
+
+    $( "#m_modal_contracts" ).on('shown.bs.modal', function(){
+        contract_table.clear();
+        contract_table.ajax.reload();
+    });
 });
 
 
@@ -50,5 +58,54 @@ function fun_edit(id) {
 }
 
 
+function fun_show_modal(id) {
+    customer_id = id ;
+    $('#m_modal_contracts').modal('show');
+    // 显示此客户对应的合同列表
 
+    contract_table = $("#contract_table").DataTable({
+            "width": "100%",
+            "autoWidth": true,
+            "ajax": {
+                url: contextPath + "contracts/" + customer_id + "/getDataWithoutPaged.json",
+                error: function (data, textStatus, jqXHR) {
+                    Logger.debug(jqXHR);
+                    showMessage("danger", "错误", jqXHR.responseJSON.data[0].errorMessage);
+                },
+            },
+            "language": {
+                "url": contextPath + "assets/js/lib/DataTables-1.10.16/chinese.lang.json"
+            },
+            "columns": [{
+                "data": "name"
+            }, {
+                "data": "amount"
+            }, {
+                "data": "beginDate"
+            }, {
+                "data": "endDate"
+            }, {
+                "data": "firstGatherDate"
+            }],
+            "order": [[2, "desc"]],
+            "columnDefs": [
+                {
+                    "orderable": false,
+                    "targets": [5],
+                    "render": function (data, type, row, meta) {
+                        // return '<button type="button" class="btn btn-outline-primary"  onclick="fun_edit(\'' + row.id + '\')">编辑</button>&nbsp;&nbsp;<button class="btn btn-outline-danger btn-sm" type="button"  onclick="fun_delete(\'' + row.id + '\')">删除</button>'
+                        return '<a href="javascript:fun_contract_edit(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-accent m-btn--icon m-btn--icon-only m-btn--pill" title="编辑"><i class="la la-edit"></i></a>' +
+                            '<a href="javascript:fun_contract_delete(\'' + row.id + '\')" class="m-portlet__nav-link btn m-btn m-btn--hover-danger m-btn--icon m-btn--icon-only m-btn--pill" title=" 删除"><i class="la la-trash"></i></a>'
+                    }
+                }],
+        });
+
+}
+
+/**
+ * 跳转到客户新建合同的界面
+ */
+function fun_create_contract() {
+     window.location=contextPath+"/customers/"+customer_id+"/contracts";
+}
 
